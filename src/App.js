b917348226115d1bed3dfaa28cff07890971a061
ginnyvt <ginny@powerkernel.com>
2023-04-24 09:44:54 +0300

    Create a unique categories array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
+import { menuList } from "./assets/data";
+
 function App() {
+	/* make new array that contains unique categories */
+	const categories = [...new Set(menuList.map((item) => item.category))];
+	console.log(categories);
 	return (
 		<>
 			<h3>Menu </h3>
